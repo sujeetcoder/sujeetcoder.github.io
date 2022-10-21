e519ec27082accb20fe99268ea4e62fa4d4c0b28
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-10-21 12:32:01 +0530

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,7 +12,6 @@ const Home = () => {
     return (
     <Box style={{margin:"0px",padding:"0px",boxSizing:"border-box",width:"100%",height:"2750px" }}  >
         <Navbar/>
-        <Test/>
         <About/>
         <br/>
         <br/>
