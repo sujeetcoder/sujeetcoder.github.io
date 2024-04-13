a3af87a71a1c777fdfb54ddf7cbc832711677c21
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2024-04-13 19:10:45 +0530

    upgrade 1

--- src/Componant/Home.jsx
+++ src/Componant/Home.jsx
@@ -7,7 +7,7 @@ import resume from "../pdf/Sujeet_Kumar_Resume.pdf"
 
 export const Home = () => {
     const toggleClick2 = () => {
-        window.open('https://drive.google.com/file/d/1mAXvUM_FT60NaGoP8ERlny-sripoR6Eu/view?usp=share_link')
+        window.open('https://drive.google.com/file/d/11E4L8vzhwp55kqcFKRBC8QJOV1LmBFqz/view?usp=sharing')
       }
     
     return (
--- src/Componant/emailJs.js
+++ src/Componant/emailJs.js
@@ -1,116 +1,176 @@
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText,
-    Input,
-    Box,
-    Textarea,
-    Button,
-    Text,
-    useToast,
-  } from '@chakra-ui/react'
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Box,
+  Textarea,
+  Button,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 
-import React, { useEffect, useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import { headline, yellow } from '../theme/color';
+import React, { useEffect, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { headline, yellow } from "../theme/color";
 
 export const ContactUs = () => {
-
-  let sendmail = localStorage.getItem("sendmail") || false
-  if(sendmail==="true"){
-      sendmail=true
+  let sendmail = localStorage.getItem("sendmail") || false;
+  if (sendmail === "true") {
+    sendmail = true;
   }
-  
-  const [input, setInput] = useState('')
-  const [name, setName] = useState('')
-  const [message,setMessage] = useState('')
-  const [emailErr,setEmailErr] = useState(true)
-  const [sendMail,setSendMail] = useState(sendmail)
-  const toast = useToast()
 
-  useEffect(()=>{
-    if(name==''){
-      setEmailErr(true)
+  const [input, setInput] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailErr, setEmailErr] = useState(true);
+  const [sendMail, setSendMail] = useState(sendmail);
+  const toast = useToast();
+
+  useEffect(() => {
+    if (name == "") {
+      setEmailErr(true);
     } else {
-      setEmailErr(false)
+      setEmailErr(false);
     }
-  },[name])
+  }, [name]);
 
-  const handleInputChange = (e) => setInput(e.target.value)
-  const handleNameChange = (e) => setName(e.target.value)
-  const handleMessChange = (e) => setMessage(e.target.value)
+  const handleInputChange = (e) => setInput(e.target.value);
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleMessChange = (e) => setMessage(e.target.value);
 
-  const isError = input === ''
-  const isError2 = name === ''
+  const isError = input === "";
+  const isError2 = name === "";
+  const isError3 = message.length > 15;
 
   const sendEmail = (e) => {
     /* e.preventDefault(); */
-    console.log("email send")
-    let data = {
-      user_name:name,
-      user_email:input,
-      message:message
+    /* easy */
+    let tempInput = "";
+    if (input.length > 14) {
+      for (let i = input.length - 1; i >= input.length - 10; i--) {
+        tempInput = tempInput + input[i];
+      }
+      tempInput = tempInput.split("").reverse().join("");
+      if (tempInput === "@gmail.com") {
+      } else {
+        return;
+      }
+    } else {
+      console.log("wrong email 2");
+      toast({
+        title: "Email not sent",
+        description: "wrong email found",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      return;
     }
-    console.log(data)
-    
+    /* easy */
+
+    console.log("email send");
+    let data = {
+      user_name: name,
+      user_email: input,
+      message: message,
+    };
+    console.log(data);
+
     /* emailjs.send("service_3uomgux","template_pvagz2o"); */
-    emailjs.send('service_3uomgux', 'template_pvagz2o', data, 'D7VO2Sz3ljOslaxDo')
-      .then((result) => {
+    emailjs
+      .send("service_3uomgux", "template_pvagz2o", data, "D7VO2Sz3ljOslaxDo")
+      .then(
+        (result) => {
           console.log(result.text);
-          localStorage.setItem("sendmail", true)
-          setSendMail(true)
+          localStorage.setItem("sendmail", true);
+          setSendMail(true);
           toast({
-            title: 'Email sent',
+            title: "Email sent",
             description: "Your email sent to Sujeet Kumar successfully",
-            status: 'success',
+            status: "success",
             duration: 9000,
             isClosable: true,
-          })
-      }, (error) => {
+          });
+        },
+        (error) => {
           console.log(error);
           toast({
-            title: 'Email not sent',
-            description: "Something went wrong please check and resend email again",
-            status: 'error',
+            title: "Email not sent",
+            description:
+              "Something went wrong please check and resend email again",
+            status: "error",
             duration: 9000,
             isClosable: true,
-          })
-      });
+          });
+        }
+      );
   };
 
-  
-  
-
   return (
-    <Box w={["80%","70%","60%","50%","40%"]} >
-    <FormControl isInvalid={isError} >
-      <FormLabel>Name</FormLabel>
-      <Input color={headline} disabled={sendMail} placeholder='Name' bgColor="white" type="text" name="user_name" value={name} onChange={handleNameChange} />
-      {!isError2 ? (
-        <FormHelperText>
-          Enter your name for future reference
-        </FormHelperText>
-      ) : (
-        <FormErrorMessage>Name is required.</FormErrorMessage>
+    <Box w={["80%", "70%", "60%", "50%", "40%"]}>
+      <FormControl isInvalid={isError}>
+        <FormLabel>Name</FormLabel>
+        <Input
+          color={headline}
+          disabled={sendMail}
+          placeholder="Name"
+          bgColor="white"
+          type="text"
+          name="user_name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        {!isError2 ? (
+          <FormHelperText>Enter your name for future reference</FormHelperText>
+        ) : (
+          <FormErrorMessage>Name is required.</FormErrorMessage>
+        )}
+        <FormLabel>Email</FormLabel>
+        <Input
+          color={headline}
+          disabled={emailErr || sendMail}
+          placeholder="Email"
+          bgColor="white"
+          type="email"
+          value={input}
+          name="user_email"
+          onChange={handleInputChange}
+        />
+        {!isError ? (
+          <FormHelperText>Enter your email for future reference</FormHelperText>
+        ) : (
+          <FormErrorMessage>Email is required.</FormErrorMessage>
+        )}
+
+        <FormLabel>Message</FormLabel>
+        <Textarea
+          onChange={handleMessChange}
+          disabled={sendMail}
+          color={headline}
+          placeholder="Message"
+          bgColor="white"
+          name="message"
+          value={message}
+        />
+        <Button
+          isDisabled={isError || isError2 || sendMail || !isError3}
+          mt="20px"
+          bgColor={yellow}
+          onClick={sendEmail}
+        >
+          {" "}
+          Send{" "}
+        </Button>
+      </FormControl>
+      {sendMail && (
+        <Text mt="20px" color={headline}>
+          {" "}
+          You allready sent a email to us. Please wait for response from my
+          side. Thanks!{" "}
+        </Text>
       )}
-      <FormLabel>Email</FormLabel>
-      <Input color={headline} disabled={emailErr||sendMail} placeholder='Email' bgColor="white"  type='email' value={input} name="user_email" onChange={handleInputChange} />
-      {!isError ? (
-        <FormHelperText>
-          Enter your email for future reference
-        </FormHelperText>
-      ) : (
-        <FormErrorMessage>Email is required.</FormErrorMessage>
-      )}
-      
-      <FormLabel>Message</FormLabel>
-      <Textarea onChange={handleMessChange} disabled={sendMail} color={headline} placeholder='Message' bgColor="white" name="message" value={message} />
-      <Button disabled={isError||isError2||sendMail} mt="20px" bgColor={yellow} onClick={sendEmail} > Send </Button>
-    </FormControl>
-    {
-    sendMail && <Text mt="20px" color={headline} > You allready sent a email to us. Please wait for response from my side. Thanks! </Text>
-    }
     </Box>
   );
 };
