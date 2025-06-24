a4acaab6e834029c6f5af972e0c652cee35a24cd
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2025-06-24 09:27:37 +0530

    ad place testing

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react"
+import { Box, Button, Image, Link, Text } from "@chakra-ui/react"
 import About from "../Componant/About.jsx";
 import { Contact } from "../Componant/Contact.jsx";
 import { HireMe } from "../Componant/HireMe.jsx";
@@ -10,26 +10,49 @@ import Test from "../Componant/Test"
 import { yellow, last, headline } from "../theme/color.js";
 import { toggleClick } from "../Toggle/ToggleClick.js";
 import arrow from "../Img/arrow.png"
+import { Helmet } from 'react-helmet';
+import { useEffect } from "react";
 
 
 
 const HomePage = () => {
     // Home, about me, skill, project, contact & resume
+
+    useEffect(() => {
+        let ads = document.createElement("script")
+            ads.type = "text/javascript"
+            ads.append("atOptions = {'key' : '0d86570bcc2b7caad76bdf2df23c0254','format' : 'iframe','height' : 90,'width' : 728,'params' : {} };")
+        
+            let ads2 = document.createElement("script")
+            ads2.type = "text/javascript"
+            ads2.src = "//www.highperformanceformat.com/0d86570bcc2b7caad76bdf2df23c0254/invoke.js"
+            let adsa = document.querySelector("#ads1")
+            adsa.append(ads)
+            adsa.append(ads2)
+            console.log(ads, ads2, adsa)
+        /* document.body.ads1.append(<h1>add places</h1>)
+        document.body.ads1.append(ads)
+        document.body.ads1.append(ads2)
+        return () => {
+            document.body.ads1.remove(<h1>add places</h1>)
+        document.body.ads1.remove(ads)
+        document.body.ads1.remove(ads2)
+        } */
+    }, [])
+
+
     return (
+        <>
+        <Helmet>
+            <script type='text/javascript' src='//pl26995538.profitableratecpm.com/bf/44/d2/bf44d2d613ce4697723efce1023a266b.js'></script>
+        </Helmet>
         <Box style={{ margin: "0px", padding: "0px", boxSizing: "border-box", width: "100%" }}  >
             <Navbar />
             <Box padding="30px 5%">
                 <Home />
-                <Box><script type="text/javascript">
-	atOptions = {
-		'key' : '0d86570bcc2b7caad76bdf2df23c0254',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/0d86570bcc2b7caad76bdf2df23c0254/invoke.js"></script></Box>
+            <Box id="ads1" >
+                {/* ads */}
+            </Box>
                 <Image
                     onClick={() => toggleClick("home")}
                     src={arrow}
@@ -44,8 +67,8 @@ const HomePage = () => {
                 <About />
                 <Skills />
                 <Projects />
+                <Link w={"100%"} textAlign={"center"} href="https://www.profitableratecpm.com/pd5iii80?key=94c3cae02b6c879d6221264036d5952d" >Mystery Box 🎁 </Link>
                 <Contact />
-                <a href="https://www.profitableratecpm.com/dauj0998h?key=28626388f1ace16b380d7adc1ef4b1e9" >Mystery Box 🎁 </a>
                 <HireMe />
 
 
@@ -58,6 +81,7 @@ const HomePage = () => {
                 <Text mt="50px"   >All copyrights reserved to © Sujeet Kumar (sujeetcoder) </Text>
             </Box>
         </Box>
+        </>
     )
 }
 
