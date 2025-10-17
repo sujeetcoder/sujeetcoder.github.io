6eacc51c00faf3efe0b72fcd8ffd4b53c285c63b
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2025-10-17 22:11:00 +0530

    minor bug fixed

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "../Pages/HomePage"
 import PagenotFound from "../Pages/PageNotFound"
-import Test2 from "../Pages/Test2"
+
 
 
 const AllRoutes = () => {
