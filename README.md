4335fda3467e95aac5edd0f122773514a438f1cf
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2026-07-14 14:43:32 +0530

    all data deleted

--- README.md
+++
@@ -1,4 +0,0 @@
-# sujeetcoder.github.io
-#Hi this is my portfolio
-
-first you visit on [homepage](https://sujeetcoder.github.io/)
--- src/Toggle/ToggleClick.js
+++
@@ -1,9 +0,0 @@
-export   function toggleClick(id){
-    console.log("done")
-    let value = document.getElementById(id)
-    let dec = 90
-    if(id=="about"){
-        dec = 160
-    }
-    window.scrollTo({top:value.offsetTop-dec,behavior:"smooth"})
- }
